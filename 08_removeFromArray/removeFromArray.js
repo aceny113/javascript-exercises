4eb04e951b133591408946658ef676d6925fc8d6
aceny113 <jasonyance13@gmail.com>
2025-10-02 22:52:44 +0800

    done 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    let newArr = [];
+    arr.forEach(item => {
+        if (!args.includes(item)) {
+            newArr.push(item);
+        };
+    });
+    return newArr;
 };
+
+console.log(removeFromArray([1,2,3,4,5], 4,5));
 
 // Do not edit below this line
 module.exports = removeFromArray;
