164ccfeb25a701ecb4c4a04d4c09db98a455344a
aceny113 <jasonyance13@gmail.com>
2025-10-02 08:59:22 +0800

    Done 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    return str.split('').reverse().join('');
 };
+console.log(reverseString("Betlog"));
+
+
+/* const reverseString = function (str) {
+    let newString = '';
+    for (let i = str.length - 1; i >= 0; i--){
+        newString += str[i];
+    }
+    return newString;
+}
+console.log(reverseString("tanginamo")); */
 
 // Do not edit below this line
 module.exports = reverseString;
