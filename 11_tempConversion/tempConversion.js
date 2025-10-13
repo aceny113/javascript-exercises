d3521cd71f5321cb1db22d56a88f396ddb18629b
aceny113 <jasonyance13@gmail.com>
2025-10-13 17:31:51 +0800

    done 11_tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,9 +1,22 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let convertFormula = (fahrenheit - 32) * 5/9;
+  const roundToOneDecimalPlaces = Math.round(convertFormula * 10) / 10;
+  return roundToOneDecimalPlaces;
 };
 
-const convertToFahrenheit = function() {
+console.log(convertToCelsius(-100));
+
+
+
+
+
+const convertToFahrenheit = function(celsius) {
+  let toFahrenheit = (celsius * 9) / 5 + 32;
+  const roundToOneDecimalPlaces = Math.round(toFahrenheit * 10) / 10;
+  return roundToOneDecimalPlaces
 };
 
+console.log(convertToFahrenheit(-10));
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
